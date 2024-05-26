876ad2cfdbf037c3b61fc711de439fbe5f00e27b
jackal7819 <jackal3166@gmail.com>
2024-05-26 23:04:44 +0300

    Add RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Button } from './components/ui/button';
+import { useAppSelector } from './hooks';
 
 const App = () => {
+	const { name } = useAppSelector((state) => state.userState);
+	console.log(name);
+  
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-			<Button variant='destructive'>Destructive</Button>
+			<h1 className='text-3xl underline'>Hello world!</h1>
+			<Button variant='destructive' className='text-base'>
+				Destructive
+			</Button>
 		</div>
 	);
 };
