3a6e28381e3c4854457f9a7da137403b01fe99f7
jackal7819 <jackal3166@gmail.com>
2024-05-29 14:31:39 +0300

    Add LinkDropdown

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -1,8 +1,50 @@
+import { NavLink } from 'react-router-dom';
+import { AlignLeft } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { links } from '@/utils/links';
+
 const LinksDropdown = () => {
 	return (
-		<div>
-			<h1>LinksDropdown</h1>
-		</div>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild className='lg:hidden'>
+				<Button variant='outline' size='icon'>
+					<AlignLeft />
+					<span className='sr-only'>Toggle Links</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent
+				className='w-32 p-2 lg:hidden'
+				align='start'
+				sideOffset={25}
+			>
+				<DropdownMenuLabel className='text-base font-normal cursor-default'>
+					Menu
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				{links.map((link) => (
+					<DropdownMenuItem key={link.label}>
+						<NavLink
+							to={link.href}
+							className={({ isActive }) => {
+								return `capitalize w-full ${
+									isActive ? 'text-primary' : ''
+								}`;
+							}}
+						>
+							<span className='capitalize'>{link.label}</span>
+						</NavLink>
+					</DropdownMenuItem>
+				))}
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 };
 
