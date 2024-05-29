373bf32e7286187501fd041969efd4941f234328
jackal7819 <jackal3166@gmail.com>
2024-05-29 22:27:45 +0300

    Add CartButton

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { ShoppingCart } from 'lucide-react';
+
 const CartButton = () => {
+	const numItemsInCart = 9;
+
 	return (
-		<div>
-			<h1>CartButton</h1>
-		</div>
+		<Button
+			asChild
+			variant='outline'
+			size='icon'
+			className='relative flex items-center justify-center'
+		>
+			<Link to='/cart'>
+				<ShoppingCart />
+				<span className='absolute flex items-center justify-center text-sm font-bold text-white rounded-full -top-3 -right-3 bg-primary size-6'>
+					{numItemsInCart}
+				</span>
+			</Link>
+		</Button>
 	);
 };
 
