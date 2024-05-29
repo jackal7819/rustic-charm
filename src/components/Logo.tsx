4f5a23d2de36e92dc5dc73e510c6d99e1a44fd40
jackal7819 <jackal3166@gmail.com>
2024-05-29 12:15:36 +0300

    Add Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Logo = () => {
 	return (
-		<div>
-			<h1>Logo</h1>
-		</div>
+		<Link to='/' className='hidden lg:flex'>
+			<div className='flex items-center justify-center p-1 text-xl font-semibold rounded-md bg-[hsl(24.6,95%,53.1%)]'>
+				<span className='ml-1 mr-2 text-white'>Rustic</span>
+				<span className='flex items-center justify-center w-20 h-8 bg-white rounded text-[hsl(24.6,95%,53.1%)]'>
+					Charm
+				</span>
+			</div>
+		</Link>
 	);
 };
 
