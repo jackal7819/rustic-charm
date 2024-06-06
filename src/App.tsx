fcc7fd9cce693a827e7b5bf4b783d56658f927c9
jackal7819 <jackal3166@gmail.com>
2024-06-06 22:14:37 +0300

    Add ErrorElement

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Error from './pages/Error';
+import ErrorElement from './components/ErrorElement'
 import HomeLayout from './pages/HomeLayout';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
@@ -21,30 +22,37 @@ const router = createBrowserRouter([
 			{
 				index: true,
 				element: <Landing />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/cart',
 				element: <Cart />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/products',
 				element: <Products />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/products/:id',
 				element: <SingleProduct />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/about',
 				element: <About />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/checkout',
 				element: <Checkout />,
+        errorElement: <ErrorElement />,
 			},
 			{
 				path: '/orders',
 				element: <Orders />,
+        errorElement: <ErrorElement />,
 			},
 		],
 	},
