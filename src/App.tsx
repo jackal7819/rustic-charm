c940da18c94037b736f31a65913a32af87d02a4a
jackal7819 <jackal3166@gmail.com>
2024-06-06 21:13:59 +0300

    Add Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,62 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import About from './pages/About'
+import About from './pages/About';
 import Cart from './pages/Cart';
-import Checkout from './pages/Checkout'
-import Error from './pages/Error'
-import HomeLayout from './pages/HomeLayout'
+import Checkout from './pages/Checkout';
+import Error from './pages/Error';
+import HomeLayout from './pages/HomeLayout';
 import Landing from './pages/Landing';
-import Login from './pages/Login'
-import Orders from './pages/Orders'
-import Products from './pages/Products'
-import Register from './pages/Register'
-import SingleProduct from './pages/SingleProduct'
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
+import Register from './pages/Register';
+import SingleProduct from './pages/SingleProduct';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/products',
-        element: <Products />,
-      },
-      {
-        path: '/products/:id',
-        element: <SingleProduct />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/checkout',
-        element: <Checkout />,
-      },
-      {
-        path: '/orders',
-        element: <Orders />,
-      },
-    ]
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+			{
+				path: '/products',
+				element: <Products />,
+			},
+			{
+				path: '/products/:id',
+				element: <SingleProduct />,
+			},
+			{
+				path: '/about',
+				element: <About />,
+			},
+			{
+				path: '/checkout',
+				element: <Checkout />,
+			},
+			{
+				path: '/orders',
+				element: <Orders />,
+			},
+		],
 	},
 	{
 		path: '/login',
 		element: <Login />,
+		errorElement: <Error />,
 	},
-  {
+	{
 		path: '/register',
 		element: <Register />,
+		errorElement: <Error />,
 	},
 ]);
 
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,8 +1,34 @@
+import { Button } from '@/components/ui/button';
+import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
 const Error = () => {
+	const error = useRouteError();
+	if (isRouteErrorResponse(error) && error.status === 404) {
+		return (
+			<main className='grid min-h-screen px-8 place-items-center'>
+				<div className='text-center'>
+					<p className='font-semibold text-9xl text-primary'>404</p>
+					<h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>
+						Page not found
+					</h1>
+					<p className='mt-6 text-lg leading-7'>
+						Sorry, we could not find the page you’re looking for
+					</p>
+					<div className='mt-10'>
+						<Button asChild size='lg' variant='secondary'>
+							<Link to='/'>Go back home</Link>
+						</Button>
+					</div>
+				</div>
+			</main>
+		);
+	}
 	return (
-		<div>
-			<h1>Error</h1>
-		</div>
+		<main className='grid min-h-screen px-8 place-items-center'>
+			<h4 className='text-4xl font-bold text-center'>
+				There was an error...
+			</h4>
+		</main>
 	);
 };
 
