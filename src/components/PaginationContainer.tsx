9d8234eb46a35e09adab9c460b41133fb524712e
jackal7819 <jackal3166@gmail.com>
2024-06-27 22:09:04 +0300

    Add pagination

--- src/components/PaginationContainer.tsx
+++ src/components/PaginationContainer.tsx
@@ -1,8 +1,60 @@
+import { ProductsResponseWithParams } from '@/utils/types';
+import { constructPrevOrNextUrl, constructUrl } from '@/utils/pagination';
+import { useLoaderData, useLocation } from 'react-router-dom';
+
+import {
+	Pagination,
+	PaginationContent,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from './ui/pagination';
+
 const PaginationContainer = () => {
+	const { meta } = useLoaderData() as ProductsResponseWithParams;
+	const { pageCount, page } = meta.pagination;
+
+	const { search, pathname } = useLocation();
+
+	const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+	if (pageCount < 2) {
+		return null;
+	}
+
+	const renderPagination = pages.map((pageNumber) => {
+		const isActive = pageNumber === page;
+		const url = constructUrl({ pageNumber, search, pathname });
+
+		return (
+			<PaginationItem key={pageNumber}>
+				<PaginationLink to={url} isActive={isActive}>
+					{pageNumber}
+				</PaginationLink>
+			</PaginationItem>
+		);
+	});
+
+	const { prevUrl, nextUrl } = constructPrevOrNextUrl({
+		currentPage: page,
+		pageCount,
+		search,
+		pathname,
+	});
+
 	return (
-		<div>
-			<h1>Pagination Container</h1>
-		</div>
+		<Pagination className='mt-16'>
+			<PaginationContent>
+				<PaginationItem>
+					<PaginationPrevious to={prevUrl} />
+				</PaginationItem>
+				{renderPagination}
+				<PaginationItem>
+					<PaginationNext to={nextUrl} />
+				</PaginationItem>
+			</PaginationContent>
+		</Pagination>
 	);
 };
 
