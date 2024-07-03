a51161c3cdd712ba2b4191861bc593bffb433212
jackal7819 <jackal3166@gmail.com>
2024-07-03 10:34:53 +0300

    Add CartTotalRow

---
+++ src/components/CartTotalRow.tsx
@@ -0,0 +1,27 @@
+import { formatAsDollars } from '@/utils/formatAsDollars';
+
+import { Separator } from './ui/separator';
+
+type CartTotalRowProps = {
+	label: string;
+	amount: number;
+	lastRow?: boolean;
+};
+
+const CartTotalRow: React.FC<CartTotalRowProps> = ({
+	label,
+	amount,
+	lastRow,
+}) => {
+	return (
+		<>
+			<p className='flex justify-between text-sm'>
+				<span>{label}</span>
+				<span>{formatAsDollars(amount)}</span>
+			</p>
+			{lastRow ? null : <Separator className='my-2' />}
+		</>
+	);
+};
+
+export default CartTotalRow;
