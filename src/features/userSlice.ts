d3eb68ffc1b14e250db736489c72d84d725b04c2
jackal7819 <jackal3166@gmail.com>
2024-07-03 20:36:00 +0300

    Add userSlice

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -1,13 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { toast } from '@/components/ui/use-toast';
 
-const initialState = {
-	name: 'user slice',
+export type User = {
+	username: string;
+	jwt: string;
+};
+
+type UserState = {
+	user: User | null;
+};
+
+const getUserFromLocalStorage = () => {
+	const user = localStorage.getItem('user');
+	return user ? JSON.parse(user) : null;
+};
+
+const initialState: UserState = {
+	user: getUserFromLocalStorage(),
 };
 
 const userSlice = createSlice({
 	name: 'user',
 	initialState,
-	reducers: {},
+	reducers: {
+		loginUser: (state, action: PayloadAction<User>) => {
+			const user = action.payload;
+			localStorage.setItem('user', JSON.stringify(user));
+			state.user = user;
+			if (user.username === 'demo user') {
+				toast({ description: 'Welcome, Guest User' });
+				return;
+			}
+			toast({ description: 'Login Successful' });
+		},
+		logoutUser: (state) => {
+			localStorage.removeItem('user');
+			state.user = null;
+		},
+	},
 });
+
+export const { loginUser, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
