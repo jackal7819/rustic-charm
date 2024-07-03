2499356bf20b2916b9da4ac10d3090aed9f9d409
jackal7819 <jackal3166@gmail.com>
2024-07-03 21:03:21 +0300

    Add Register

---
+++ src/components/SubmitBtn.tsx
@@ -0,0 +1,9 @@
+const SubmitBtn = () => {
+	return (
+		<div>
+			<h1>Submit</h1>
+		</div>
+	);
+};
+
+export default SubmitBtn;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,21 @@
+import FormInput from '@/components/FormInput'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form } from 'react-router-dom'
+
 const Register: React.FC = () => {
 	return (
-		<div>
-			<h1>Register</h1>
-		</div>
+		<section className='grid h-screen place-items-center'>
+			<Card className='w-96 bg-muted'>
+				<CardHeader>
+					<CardTitle className='tracking-wider text-center'>Register</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Form>
+						<FormInput type='text' label='username' name='username' />
+					</Form>
+				</CardContent>
+			</Card>
+		</section>
 	);
 };
 
