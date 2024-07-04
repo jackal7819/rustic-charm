450d3053b5b9d8c35ec4a345f730db0ef9ec9347
jackal7819 <jackal3166@gmail.com>
2024-07-04 20:25:25 +0300

    Add Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,52 @@
-import FormInput from '@/components/FormInput'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Form } from 'react-router-dom'
+import FormInput from '@/components/FormInput';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Form, Link } from 'react-router-dom';
 
 const Register: React.FC = () => {
 	return (
 		<section className='grid h-screen place-items-center'>
 			<Card className='w-96 bg-muted'>
 				<CardHeader>
-					<CardTitle className='tracking-wider text-center'>Register</CardTitle>
+					<CardTitle className='tracking-wider text-center'>
+						Register
+					</CardTitle>
 				</CardHeader>
 				<CardContent>
 					<Form>
-						<FormInput type='text' label='username' name='username' />
+						<FormInput
+							type='text'
+							defaultValue='test'
+							name='username'
+						/>
+						<FormInput
+							type='email'
+							defaultValue='test@test.com'
+							name='email'
+						/>
+						<FormInput
+							type='password'
+							defaultValue='secret'
+							name='password'
+						/>
+						<Button
+							type='submit'
+							variant='default'
+							className='w-full mt-4 capitalize'
+						>
+							register
+						</Button>
+						<p className='mt-4 text-center'>
+							Already a member?{' '}
+							<Button type='button' variant='link'>
+								<Link
+									to='/login'
+									className='text-lg capitalize'
+								>
+									login
+								</Link>
+							</Button>
+						</p>
 					</Form>
 				</CardContent>
 			</Card>
