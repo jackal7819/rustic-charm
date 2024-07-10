ad7cba930cc55567f3e5b61c244694bbe2503d42
jackal7819 <jackal3166@gmail.com>
2024-07-10 19:32:10 +0300

    Add Orders

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,8 +1,23 @@
+import ComplexPaginationContainer from '@/components/ComplexPaginationContainer';
+import OrdersList from '@/components/OrdersList';
+import SectionTitle from '@/components/SectionTitle';
+import { OrdersResponse } from '@/utils/types';
+import { useLoaderData } from 'react-router-dom';
+
 const Orders: React.FC = () => {
+	const { meta, data } = useLoaderData() as OrdersResponse;
+	console.log(data);
+
+	if (meta.pagination.total < 1) {
+		return <SectionTitle text='Please make an order' />;
+	}
+
 	return (
-		<div>
-			<h1>Orders</h1>
-		</div>
+		<>
+			<SectionTitle text='Your Orders' />
+			<OrdersList />
+			<ComplexPaginationContainer />
+		</>
 	);
 };
 
