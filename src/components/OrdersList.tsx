a889b0a2245934c51dffe6de411408434ffd8ca7
jackal7819 <jackal3166@gmail.com>
2024-07-10 20:07:16 +0300

    Add OrdersList

--- src/components/OrdersList.tsx
+++ src/components/OrdersList.tsx
@@ -1,7 +1,61 @@
+import { OrdersResponse } from '@/utils/types';
+import { useLoaderData } from 'react-router-dom';
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from './ui/table';
+
 const OrdersList: React.FC = () => {
+	const { meta, data: orders } = useLoaderData() as OrdersResponse;
+
 	return (
-		<div>
-			<h1>OrdersList</h1>
+		<div className='mt-16'>
+			<h4 className='mb-4 capitalize'>
+				total orders : {meta.pagination.total}
+			</h4>
+			<Table>
+				<TableCaption>A list of your recent orders</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Name</TableHead>
+						<TableHead>Address</TableHead>
+						<TableHead className='w-24'>Products</TableHead>
+						<TableHead className='w-24 text-center'>Cost</TableHead>
+						<TableHead>Date</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{orders.map((order) => {
+						const {
+							name,
+							address,
+							numItemsInCart,
+							orderTotal,
+							createdAt,
+						} = order.attributes;
+						return (
+							<TableRow key={order.id}>
+								<TableCell>{name}</TableCell>
+								<TableCell>{address}</TableCell>
+								<TableCell className='text-center'>
+									{numItemsInCart}
+								</TableCell>
+								<TableCell className='text-right'>
+									{orderTotal}
+								</TableCell>
+								<TableCell>
+									{new Date(createdAt).toDateString()}
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
 		</div>
 	);
 };
